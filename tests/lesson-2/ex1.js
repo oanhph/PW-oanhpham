86c8e1496e5fe42878fc53f1ab2ebb37280660c1
Oanh Pham <oanhpham@Oanhs-MacBook-Air.local>
2025-03-20 17:52:13 +0700

    feat: add ex1.js, ex2.js, ex3.js

---
+++ tests/lesson-2/ex1.js
@@ -0,0 +1,7 @@
+const number = 12;
+let name = "my number";
+let isEven = false;
+
+if (12 % 2 === 0) {
+    console.log(isEven = true);
+}
---
+++ tests/lesson-2/ex3.js
@@ -0,0 +1,13 @@
+const number = 22;
+
+if (22 > 0) {
+    console.log("Giá trị bạn nhập là số dương");
+};
+
+if (22 < 0) {
+    console.log("Giá trị bạn nhập là số âm");
+};
+
+if (22 === 0) {
+    console.log("Giá trị bạn nhập là số 0");
+}
