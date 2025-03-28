683ca75711955e4a35c07deda681598a2ec08e89
Oanh Pham <oanhpham9597@gmail.com>
2025-03-28 13:31:38 +0700

    feat: homework 02-function/ex4.js

--- tests/lesson-3/02-function/ex3.js
+++ tests/lesson-3/02-function/ex3.js
@@ -4,21 +4,21 @@ Biết:
 ○ Các số nguyên tố là số chỉ chia hết cho 1 và chính nó
 */
 
-let number = [1, 5, 3, 4, 7, 8, 10];
+let number = [0, 1, 5, 3, 4, 7, 8, 10, 15, 2];
 let result = [];
 
-function isPrime(num) {
-    if (num < 2) return false; 
-    if (num === 2 || num === 3) return true; 
-    if (num % 2 === 0) return false; 
+function filterPrime(num) {
+    if (num < 2) return false;
+    if (num === 2) return true;
+    if (num % 2 === 0) return false;
 
-    for (let i = 3; i <= num / 2; i += 2) { 
+    for (let i = 2; i <= num / 2; i++) {
         if (num % i === 0) return false;
     }
     return true;
 };
 number.forEach(num => {
-    if (isPrime(num)) { 
+    if (filterPrime(num)) {
         result[result.length] = num;
     }
 });
