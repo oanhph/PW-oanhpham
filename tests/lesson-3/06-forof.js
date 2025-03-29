a005fb21850f8bb64cdd4a08753b4df4c51e7489
Oanh Pham <oanhpham9597@gmail.com>
2025-03-29 18:58:44 +0700

    feat: homework 06-forof.js

---
+++ tests/lesson-3/06-forof.js
@@ -0,0 +1,19 @@
+// 1. Tạo mảng chứa các kí tự nghịch đảo từ một chuỗi đã cho
+let arrStr = "Playwright";
+let inversion = [];
+
+for (let i = arrStr.length - 1; i >= 0; i--) {
+    inversion.push(arrStr[i]);
+}
+console.log(inversion);
+
+// 2. Lọc ra tất cả các phần tử duy nhất trong một mảng
+let arrNumber = [1, 2, 3, 1, 2, 4, 5];
+let unique = [];
+
+for (let num of arrNumber) {
+    if (arrNumber.indexOf(num) === arrNumber.lastIndexOf(num)) {
+        unique.push(num);
+    }
+}
+console.log(unique);
