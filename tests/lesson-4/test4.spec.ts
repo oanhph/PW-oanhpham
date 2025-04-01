2f42da50bda9e7b5b72caace9c74bae406fc3045
Oanh Pham <oanhpham9597@gmail.com>
2025-04-01 16:29:08 +0700

    chore: add cmt to test4.spec.ts

--- tests/lesson-4/test4.spec.ts
+++ tests/lesson-4/test4.spec.ts
@@ -1,22 +1,23 @@
-import { test, expect} from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
-test("Add 10 notes and search title", async ({page}) => {
+test("Add 10 notes and search title", async ({ page }) => {
     await page.goto("https://material.playwrightvn.com/");
     await expect(page).toHaveTitle(/Tài liệu học automation test/);
-
-    // function addNote(noteTitle, noteConitent) {
-    //     s
-    // }
 
     await page.click("//a[@href='04-xpath-personal-notes.html']");
     await expect(page).toHaveTitle(/Personal Notes/);
 
+    // Add note
     let dataTest4 = require('./data-lesson-4.json');
     for (let record of dataTest4) {
         await page.fill("//input[@id='note-title']", record.noteTitle);
         await page.fill("//textarea[@id='note-content']", record.noteContent);
+        await page.click("//button[@id='add-note']");
     };
-    
-    await page.click("//button[@id='add-note']");
-     
+
+    // Search via title
+    await page.locator("//input[@id='search']").pressSequentially("Việt Nam - Indonesia ký ý định thư về khoa học công nghệ", {
+        delay: 100
+    });
+
 });
