77b1a82f1d1d5937d5d128d40222a0c12e5ffe95
Oanh Pham <oanhpham9597@gmail.com>
2025-04-01 14:02:03 +0700

    feat: homework test3.spec.ts

---
+++ tests/lesson-4/test3.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+test("Add 100 items", async ({ page }) => {
+    await page.goto("https://material.playwrightvn.com/");
+    await expect(page).toHaveTitle(/Tài liệu học automation test/);
+
+    await page.click("//a[@href='03-xpath-todo-list.html']");
+    await expect(page).toHaveTitle(/To-Do List/);
+
+    for (let i = 1; i <= 100; i++) {
+        await page.fill("//input[@id='new-task']", "Todo <i>");
+        await page.click("//button[@id='add-task']");
+    };
+
+    page.on("dialog", async dialog => dialog.accept());
+    for (let i = 1; i <= 100; i += 2) {
+        await page.click("//button[@id='todo-i--delete']");
+    };
+});
