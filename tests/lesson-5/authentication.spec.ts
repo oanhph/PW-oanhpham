870b4f1df3a0e359a253ec9e146f011f8eda4cec
Oanh Pham <oanhpham9597@gmail.com>
2025-04-09 22:36:28 +0700

    chore: update xpath

--- tests/lesson-5/authentication.spec.ts
+++ tests/lesson-5/authentication.spec.ts
@@ -1,5 +1,9 @@
 import { test, expect } from "@playwright/test";
 
+let xpathUserName = "//input[@id='user_login']";
+let xpathPassword = "//input[@id='user_pass']";
+let xpathBtnLogin = "//input[@id='wp-submit']";
+
 const usernameInvalid = "frank";
 const passwordInvalid = "123456";
 const usernameValid = "k11-trang";
@@ -13,12 +17,12 @@ test.describe("AUTH - Authentication", async () => {
 
     test("@AUTH_001: Login fail", async ({ page }) => {
         await test.step("Input invalid values to username, password", async () => {
-            await page.locator("//input[@id='user_login']").fill(usernameInvalid);
-            await page.locator("//input[@id='user_pass']").fill(passwordInvalid);
+            await page.locator(xpathUserName).fill(usernameInvalid);
+            await page.locator(xpathPassword).fill(passwordInvalid);
         });
 
         await test.step("Submit invalid data", async () => {
-            await page.click("//input[@id='wp-submit']");
+            await page.click(xpathBtnLogin);
 
             await expect(page.getByText("Error: The username frank is not registered on this site. If you are unsure of your username, try your email address instead.")).toBeVisible();
         });
@@ -27,12 +31,12 @@ test.describe("AUTH - Authentication", async () => {
 
     test("@AUTH_002: Login success", async ({ page }) => {
         await test.step("Input valid values", async () => {
-            await page.locator("//input[@id='user_login']").fill(usernameValid);
-            await page.locator("//input[@id='user_pass']").fill(passwordValid);
+            await page.locator(xpathUserName).fill(usernameValid);
+            await page.locator(xpathPassword).fill(passwordValid);
         });
 
         await test.step("Submit valid data", async () => {
-            await page.click("//input[@id='wp-submit']");
+            await page.click(xpathBtnLogin);
 
             await expect(page).toHaveURL(/wp-admin/);
             await expect(page.getByRole("heading", { name: "Dashboard" })).toBeVisible();
