5d90725bd65142e7365931bcfaecfc11d6587e55
Oanh Pham <oanhpham9597@gmail.com>
2025-04-10 17:00:54 +0700

    feat: add key-takeaways & 01-pom & test1

---
+++ tests/lesson-6/01-pom.ts
@@ -0,0 +1,66 @@
+import { Locator, Page } from "@playwright/test";
+
+export class MaterialBasePage {
+    page: Page;
+    xpathRegisterPage: string;
+    xpathProductPage: string;
+    cssTodoPage: string;
+    personalNote: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.xpathRegisterPage = "//a[@href='01-xpath-register-page.html']";
+        this.xpathProductPage = "//a[@href='02-xpath-product-page.html']";
+        this.cssTodoPage = "a[href='03-xpath-todo-list.html']";
+    }
+
+    async openMaterialPage(url: string) {
+        await this.page.goto(url);
+    }
+
+    async gotoPage(pageName: string) {
+        switch (pageName.toLowerCase()) {
+            case "register":
+                await this.page.locator(this.xpathRegisterPage).click();
+                break;
+            case "product":
+                await this.page.locator(this.xpathProductPage).click();
+                break;
+            case "todo":
+                await this.page.locator(this.cssTodoPage).click();
+                break;
+        }
+    }
+}
+
+export class RegisterPage extends MaterialBasePage {
+    xpathUserName: string;
+    xpathEmail: string;
+    xpathGenderMale: string;
+    xpathGenderFemale: string;
+
+    constructor(page: Page) {
+        super(page);
+        this.xpathUserName = "//input[@id='username']";
+        this.xpathEmail = "//input[@id='email']";
+        this.xpathGenderFemale = "//input[@id='female']";
+        this.xpathGenderMale = "//input[@id='male']";
+    }
+
+    async fillUserName(username: string) {
+        await this.page.locator(this.xpathUserName).fill(username);
+    }
+
+    async fillEmail(email: string) {
+        await this.page.locator(this.xpathEmail).fill(email);
+    }
+
+    async checkGender(gender: string) {
+        if (gender.toLocaleLowerCase() === "male") {
+            await this.page.locator(this.xpathGenderMale).check();
+        }
+        else if (gender.toLocaleLowerCase() === "female") {
+            await this.page.locator(this.xpathGenderFemale).check();
+        }
+    }
+}
