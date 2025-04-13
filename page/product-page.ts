228a2ed879100f98ff20ea62484d93a7fd7cab87
Oanh Pham <oanhpham9597@gmail.com>
2025-04-13 18:47:02 +0700

    feat: test2.spec.ts & product-page.ts

--- page/product-page.ts
+++ page/product-page.ts
@@ -6,7 +6,6 @@ export class ProductPage extends MaterialBasePage {
     constructor(page: Page) {
         super(page);
     }
-
     // Generate xpath based on product ID
     getAddButtonXpathByProductId(productId: number): string {
         return `//button[@class='add-to-cart' and @data-product-id='${productId}']`;
@@ -28,54 +27,4 @@ export class ProductPage extends MaterialBasePage {
         }
     }
 
-    // Veridy items in card
-    async verifyCartItems(productNameToQuantity: Record<string, number>) {
-        for (const [productName, expectedQuantity] of Object.entries(productNameToQuantity)) {
-            const xpathQuantity = `//tbody[@id="cart-items"]/tr[td[1][text()="${productName}"]]/td[3]`;
-            let actualQuantityText = await this.page.textContent(xpathQuantity);
-            let actualQuantity = Number(actualQuantityText);
-
-            if (expectedQuantity === actualQuantity) {
-                console.log("Correct quantity");
-            }
-            else {
-                console.log("Incorrect quantity");
-            }
-        }
-    }
-
-    // Verify total
-    async verifyTotalFromProductConfig(productConfig: Record<number, number>) {
-        let priceTable: Record<number, number> = {
-            1: 10,
-            2: 20,
-            3: 30
-        }
-        let expectedTotal = 0;
-        for (const [productId, quantity] of Object.entries(productConfig)) {
-            let id = Number(productId);
-            let productPrice = priceTable[id];
-
-            expectedTotal += productPrice * quantity;
-        }
-        const xpathTotal = "//tfoot/tr/td[2]";
-        const totalText = await this.page.textContent(xpathTotal);
-        const actualTotal = Number(totalText);
-
-        if (expectedTotal === actualTotal) {
-            console.log("Correct total");
-        }
-        else {
-            console.log("Incorrect total");
-        }
-    }
 }
-
-
-/** verifyCartTotal(productNameToTotal) - productName, expectedTotal
-    xpathTotalEachItem
-    totalEachItemText 
-    totalEachItem 
-    expectedTotal 
-    actualTotal = 
-*/
--- tests/lesson-6/test2.spec.ts
+++ tests/lesson-6/test2.spec.ts
@@ -2,17 +2,7 @@ import { test, expect } from "@playwright/test";
 import { ProductPage } from "../../page/product-page";
 
 test("Ex2: Product Page", async ({ page }) => {
-    let productPage = new ProductPage(page);
-    let productConfig = {
-        1: 2,
-        2: 3,
-        3: 1
-    };
-    let productNameToQuantity = {
-        "Product 1": 2,
-        "Product 2": 3,
-        "Product 3": 1
-    };
+    const productPage = new ProductPage(page);
 
     await test.step("Navigate to Product page", async () => {
         await productPage.openMaterialPage("https://material.playwrightvn.com/");
@@ -20,14 +10,11 @@ test("Ex2: Product Page", async ({ page }) => {
     })
 
     await test.step("Add product", async () => {
+        let productConfig = {
+            1: 2,
+            2: 3,
+            3: 1
+        }
         await productPage.addProductsFromConfig(productConfig);
-    })
-
-    await test.step("Verify items in card", async () => {
-        await productPage.verifyCartItems(productNameToQuantity);
-    })
-
-    await test.step("Verify total", async () => {
-        await productPage.verifyTotalFromProductConfig(productConfig);
     })
 })
