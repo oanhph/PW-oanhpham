936e67c7310c4dc93a0e7aba51b63d20dcbe8b51
Oanh Pham <oanhpham9597@gmail.com>
2025-04-15 13:55:56 +0700

    chore: update locator in todo-page.ts

--- page/todo-page.ts
+++ page/todo-page.ts
@@ -10,8 +10,7 @@ export class TodoPage extends MaterialBasePage {
         super(page);
         this.inputTask = page.locator("//input[@id='new-task']");
         this.btnAddTask = page.locator("//button[@id='add-task']");
-        // this.listTask = page.locator("//ul[@id='task-list']");
-        this.listTask = page.locator("//li");
+        this.listTask = page.locator("//ul[@id='task-list']");
         // this.btnDeleteTask = page.locator(`//button[@id='todo-${i}-delete']`);
     }
 
@@ -28,54 +27,31 @@ export class TodoPage extends MaterialBasePage {
     //     return this.listTask.locator("//li"); 
     // }
 
-    // Delete odd numbered items
-    async deleteOddTasks() {
-        // this.page.on("dialog", async dialog => dialog.accept());
-
-        // await this.listTask.first().waitFor({ state: "visible" });
-
-        // let countTasks = await this.listTask.elementHandles();
-
-        // for (let i = 0; i < countTasks.length; i += 2) {
-        //     countTasks = await this.listTask.elementHandles();
-        //     console.log(`Lần lặp i = ${i}, số task hiện tại: ${countTasks.length}`);
-
-
-        //     const oddTask = countTasks[i];
-        //     console.log(countTasks);
-        //     const btnOddDelete = await oddTask.$("button[id$='-delete']");
-        //     if (btnOddDelete !== null) {
-        //         await btnOddDelete.click();
-        //     }
-        // }
-        this.page.on("dialog", async dialog => dialog.accept());
-        let countTasks = await this.listTask.count();
-        for (let i = countTasks - 1; i >= 0; i -= 2) {
-            let oddTask = this.listTask.nth(i);
-            let btnOddDelete = oddTask.locator("button[id$='-delete']").first();
-            await btnOddDelete.click();
-            await this.page.waitForTimeout(500);
-            countTasks = await this.listTask.count();
-        };
+    // Get all tasks
+    async getAllTasks() {
+        return this.listTask.locator("//li"); 
     }
+
+
+
 
     // Verify task is in viewport
 
     // async verifyTaskInViewport(taskIndex: number) {
     //     const taskLocator = (await this.getAllTasks()).nth(taskIndex);
     //     const taskHandle = await taskLocator.elementHandle() as ElementHandle<HTMLElement>;
-
+    
     //     if (!taskHandle) {
     //         console.log(`Todo ${taskIndex + 1} không tồn tại hoặc không hiển thị.`);
     //         return;
     //     }
-
+    
     //     const isInViewport = await taskHandle.isInViewport();
-
+    
     //     console.log(`Todo ${taskIndex + 1} is ${isInViewport ? "" : "NOT "}in the viewport.`);
     // }
-
-
+    
+    
 
 
 
@@ -96,6 +72,23 @@ export class TodoPage extends MaterialBasePage {
 
 
 
+    // Delete odd numbered items
+    // async deleteOddTasks() {
+    //     // this.page.on("dialog", async dialog => dialog.accept());
+
+    //     let countTasks = await this.listTask.elementHandles();
+
+    //     for (let i = 0; i < countTasks.length; i += 2) {
+    //         countTasks = await this.listTask.elementHandles();
+            
+    //         const oddTask = countTasks[i];
+    //         console.log(countTasks);
+    //         const btnOddDelete = await oddTask.$("button[id$='-delete']");
+    //         if (btnOddDelete !== null) {
+    //             await btnOddDelete.click();
+    //         }
+    //     }
+    // }
 
 
 
@@ -106,4 +99,12 @@ export class TodoPage extends MaterialBasePage {
 
 
 
-
+// this.page.on("dialog", async dialog => dialog.accept());
+// let countTasks = await this.listTask.count();
+// for (let i = countTasks -1 ; i>=0; i -= 2) {
+//     let oddTask = this.listTask.nth(i);
+//     let btnOddDelete = oddTask.locator("button[id$='-delete']").first();
+//     await btnOddDelete.click();
+//     await this.page.waitForTimeout(500);
+//         countTasks = await this.listTask.count();
+// };
