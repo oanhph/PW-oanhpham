e01e2737b6862a8be03dc63cf9eaccfe2ec335f5
Oanh Pham <oanhpham9597@gmail.com>
2025-05-26 15:31:06 +0700

    feat: update test case - post.ver-3.spec.ts

--- tests/lesson-5/post-ver-3.spec.ts
+++ tests/lesson-5/post-ver-3.spec.ts
@@ -70,25 +70,15 @@ test.describe("POST - Post tag failed", async () => {
             await expect(page.locator(xpathMsgExisTagName)).toBeVisible();
         })
     });
-
-
-
-    // await test.step("Remove tag", async () => {
-    //     page.on("dialog", async dialog => dialog.accept());
-
-    //     await page.locator(getXpathNameInTable(name1)).hover();
-    //     await page.locator(getXpathBtnDelete(name1)).click();
-    //     await expect(page.locator(getXpathNameInTable(name1))).toBeHidden();
-
-    //     await page.locator(getXpathNameInTable(name2)).hover();
-    //     await page.locator(getXpathBtnDelete(name2)).click();
-    //     await expect(page.locator(getXpathNameInTable(name2))).toBeHidden();
-    // });
-
-
 });
 
 test.describe("POST - Add Tag success", async () => {
+    let name1 = "tag Oanh Pham 01";
+    let name2 = "tag Oanh Pham 02";
+    let slug2 = "oanh-pham-02";
+    let name3 = "tag Oanh Pham 03";
+    let slug3 = "Đây là tag đặc biệt @100 $200";
+
     test.beforeEach(async ({ page }) => {
         await test.step("Go to Tag page", async () => {
             await page.goto("https://pw-practice-dev.playwrightvn.com/wp-admin");
@@ -118,10 +108,7 @@ test.describe("POST - Add Tag success", async () => {
         await expect(page.locator(getXpathNameInTable(name3))).toBeHidden();
     });
 
-    let name1 = "tag Oanh Pham";
-    let name2 = "Rennie Pham";
-    let slug2 = "rennie-pham";
-    test("@POST_TAG_002: Tag - add tag success", async ({ page }) => {
+    test("@POST_TAG_002 & @POST_TAG_003: Tag - add tag success", async ({ page }) => {
 
         await test.step("Submit valid name", async () => {
             await page.locator(xpathInputName).fill(name1);
@@ -140,11 +127,8 @@ test.describe("POST - Add Tag success", async () => {
             await expect(page.locator(getXpathNameInTable(name2))).toBeVisible();
             await expect(page.locator(getXpathSlugInTable(slug2))).toBeVisible();
         });
-    });
-    let name3 = "tag Oanh Pham 03";
-    let slug3 = "Đây là tag đặc biệt @100 $200";
-    test("@POST_TAG_003: Tag add tag with special characters in slug", async ({ page }) => {
-        test.step("Submit special tag", async () => {
+
+        await test.step("Submit special tag", async () => {
             await page.locator(xpathInputName).fill(name3);
             await page.locator(xpathInputSlug).fill(slug3);
             await page.click(xpathBtnAddNew);
@@ -152,13 +136,9 @@ test.describe("POST - Add Tag success", async () => {
             await expect(page.locator(xpathMsgTagAdded)).toBeVisible();
             await expect(page.locator(getXpathNameInTable(name3))).toBeVisible();
             await expect(page.locator(getXpathSlugInTable(slug3))).toBeVisible();
-        })
-    })
-})
-
-
-
-
+        });
+    });
+});
 
 test.describe("POST - Category", async () => {
     let arrCategories = [
