141ea130dba28fbd609dfe3f40bd3d0c0f3b664d
Oanh Pham <oanhpham9597@gmail.com>
2025-06-08 20:59:07 +0700

    feat: add function to personal-not-page.ts

--- page/personal-note-page.ts
+++ page/personal-note-page.ts
@@ -2,8 +2,24 @@ import { Page } from "@playwright/test";
 import { MaterialBasePage } from "./base-page";
 
 export class PersonalNote extends MaterialBasePage {
+    xpathTitle = "//input[@type='text' and @id='note-title']";
+    xpathContent = "//textarea[@id='note-content']";
+    xpathBtnAddNote = "//button[@id='add-note']";
+    xpathSearchNotes = "//input[@type= 'text' and @id= 'search']";
 
     constructor(page: Page) {
         super(page);
+    }
+
+    async fillTitle(title: string) {
+        await this.page.locator(this.xpathTitle).fill(title);
+    }
+
+    async fillContent(content: string) {
+        await this.page.locator(this.xpathContent).fill(content);
+    }
+
+    async clickAddNote() {
+        await this.page.locator(this.xpathBtnAddNote).click();
     }
 }
