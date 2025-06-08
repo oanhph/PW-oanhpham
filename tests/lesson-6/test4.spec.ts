c8be3a2e1fa2fe85ff5f284b66a4fd691ea93fd0
Oanh Pham <oanhpham9597@gmail.com>
2025-06-08 21:21:30 +0700

    feat: add step add 5 notes - test4.spec.ts

--- tests/lesson-6/test4.spec.ts
+++ tests/lesson-6/test4.spec.ts
@@ -1,14 +1,14 @@
 import test from "@playwright/test";
 import { PersonalNote } from "../../page/personal-note-page";
 
-test("Ex4: Personal note", async ({page}) => {
+test("Ex4: Personal note", async ({ page }) => {
     let personalNote = new PersonalNote(page);
     let titles, contents;
 
     await test.step("Clone data from VNExpress", async () => {
-        await page.goto('https://vnexpress.net/khoa-hoc', { waitUntil: 'domcontentloaded' });
-        titles = await page.locator('//h3[@class="title-news"]/a').allTextContents();
-        contents = await page.locator('//p[@class="description"]/a').allTextContents();
+        await page.goto("https://vnexpress.net/khoa-hoc", { waitUntil: "domcontentloaded" });
+        titles = await page.locator("//h3[@class='title-news']/a").allTextContents();
+        contents = await page.locator("//p[@class='description']/a").allTextContents();
         console.log(titles);
     })
 
@@ -16,4 +16,13 @@ test("Ex4: Personal note", async ({page}) => {
         await personalNote.openMaterialPage("https://material.playwrightvn.com/");
         await personalNote.gotoPage("personal note");
     })
+
+    await test.step("Add 5 notes", async () => {
+        for (let i = 0; i <= 4; i++) {
+            await personalNote.fillTitle(titles[i]);
+            await personalNote.fillContent(contents[i]);
+            await personalNote.clickAddNote()
+        }
+    })
+    //     let notes: { title: string, content: string }[] = [];
 })
